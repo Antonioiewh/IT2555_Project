e49cdb09afb7f7c4cefc24c39666ed01eb8e9633
Lahsyv <Svishaal256@gmail.com>
2025-04-22 13:59:24 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 #complete the function below to compute the product of X and Y
 def multiply(x,y):
     #code should be here
-
+    return x*y
 
 #complete the function below to compute X divided by Y
 def divide(x,y):
-
+    return x/y
 
 #complete the function below to compute X to the power of Y
 def power(x,y):
+    return x**y
 
 
+print(multiply(3,4))
 
-multiply(3,4)
-divide(314,2)
-power(5,7)
+print(divide(314,2))
+
+print(power(5,7))
 

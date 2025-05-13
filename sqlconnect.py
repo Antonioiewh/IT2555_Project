11c3bdb05be82980a4a33989e2e5b75036cd568b
Antonioiewh <nghwei1@gmail.com>
2025-05-13 15:50:28 +0800

    Delete sqlconnect.py

--- sqlconnect.py
+++
@@ -1,5 +0,0 @@
-mysql_host ='localhost'
-mysql_user ='root'
-#This is my password, but you should change it to your own password that you chose when setting up MySQL
-mysql_password='hermos'
-mysql_name = 'simplebook'

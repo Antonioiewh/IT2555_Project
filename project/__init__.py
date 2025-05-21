488845250db7af644e99f8d2ea7d4bbf5d09ac27
Antonioiewh <nghwei1@gmail.com>
2025-05-22 01:45:41 +0800

    fucker ctrl-c -> docker build and docker up

--- project/__init__.py
+++ project/__init__.py
@@ -11,6 +11,7 @@ import cryptography
 #antonio: this is the connection string to connect to mysql
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 '''
 app.config['SQLALCHEMY_DATABASE_URI'] = conn
 app.permanent_session_lifetime = timedelta(minutes=5)`
@@ -38,14 +39,16 @@ def index():
     test_list = []
     for row in rs:
         test_list.append(row)
-        print(row)
     return render_template('index.html', test_list=test_list)
+
 
 
 #antonio: i forgot what this does, but it is important to have it here
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
+    
+
     #determine the create the obj
     app.run(debug=True)
 
